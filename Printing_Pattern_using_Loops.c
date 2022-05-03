97ed577396e56cf5551088241577886fe8be889c
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2022-05-03 16:13:39 +0530

    #include<stdio.h> int main() {     int i,j,k,m,n,x;     scanf("%d",&n);     k=n;     m=n+(n-1);     for(i=0;i<m;i++)     {         for(j=0;j<m;j++)         {             if(i<=n-1)             {                 if(i==0)                 {                     printf("%d ",k);                 }                 if(i>=1)                 {                     if(j<i)                     {                         printf("%d ",k-j);                     }                     else if(j>=1&&j<m-i)                     {                         printf("%d ",k-i);                     }                     else                     {                         printf("%d ",(j-k+1)+1);                     }                 }             }             else if(i==n-1)             {                 if(j<n)                 {                     printf("%d ",k-j);                 }                 else                 {                     printf("%d ",(j-k+1)+1);                 }             }             else if(i>=n)             {                 x=m-i-1;                 if(i==m)                 {                     printf("%d ",k);                 }                 if(j<x)                 {                     printf("%d ",k-j);                 }                 else if(j>=x&&j<m-x)                 {                     printf("%d ",k-x);                 }                 else                 {                     printf("%d ",(j-k+1)+1);                 }             }         }         printf(" ");     } }

---
+++ Printing_Pattern_using_Loops.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,m,n,x;
+    scanf("%d",&n);
+    k=n;
+    m=n+(n-1);
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            if(i<=n-1)
+            {
+                if(i==0)
+                {
+                    printf("%d ",k);
+                }
+                if(i>=1)
+                {
+                    if(j<i)
+                    {
+                        printf("%d ",k-j);
+                    }
+                    else if(j>=1&&j<m-i)
+                    {
+                        printf("%d ",k-i);
+                    }
+                    else
+                    {
+                        printf("%d ",(j-k+1)+1);
+                    }
+                }
+            }
+            else if(i==n-1)
+            {
+                if(j<n)
+                {
+                    printf("%d ",k-j);
+                }
+                else
+                {
+                    printf("%d ",(j-k+1)+1);
+                }
+            }
+            else if(i>=n)
+            {
+                x=m-i-1;
+                if(i==m)
+                {
+                    printf("%d ",k);
+                }
+                if(j<x)
+                {
+                    printf("%d ",k-j);
+                }
+                else if(j>=x&&j<m-x)
+                {
+                    printf("%d ",k-x);
+                }
+                else
+                {
+                    printf("%d ",(j-k+1)+1);
+                }
+            }
+        }
+        printf("
+");
+    }
+}
