eaeab7386b0ece34b08461bcc19c4b4824ea37bf
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2022-05-21 22:13:38 +0530

    #include<stdio.h> int main() {     int n,c1=0,c,a[100],i,j,d;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                 c++;             }         }         if(c>c1)         {             c1=c;             d=a[i];         }         if(c==c1)         {             if(d>a[i])             {                 d=a[i];             }         }     }     printf("%d",d); }

---
+++ Majority_Element.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int n,c1=0,c,a[100],i,j,d;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(c>c1)
+        {
+            c1=c;
+            d=a[i];
+        }
+        if(c==c1)
+        {
+            if(d>a[i])
+            {
+                d=a[i];
+            }
+        }
+    }
+    printf("%d",d);
+}
